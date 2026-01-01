e76b476847af9cacfba4f6653f549483b5a0eb10
shir levi <shirlevi66@gmail.com>
2026-01-01 15:21:49 +0200

    now

---
+++ Test.java
@@ -0,0 +1,6 @@
+public class Test {
+    public static void main(String[] args) {
+        // This file is required by the autograder build script.
+        // The real tests are run separately.
+    }
+}
